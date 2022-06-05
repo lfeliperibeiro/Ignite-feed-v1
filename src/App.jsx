f141476a6ea5860e017c8b1c6188eaa00e94f623
Felipe Ribeiro <piporibeiro@gmail.com>
2022-06-05 19:52:30 -0300

    feat: adjust home structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { Header } from './components/Header';
+import styles from './App.module.css';
+import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author={'Felipe Ribeiro'}
+            content={'lorem implements dolor lore'}
+          />
+          <Post
+            author={'Felipe Ribeiro'}
+            content={'lorem implements dolor lore'}
+          />
+        </main>
+      </div>
     </div>
   );
 }
