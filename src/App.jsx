a0332bb3b710085563a5824822a9999ddd541e32
Felipe Ribeiro <piporibeiro@gmail.com>
2022-06-09 18:35:22 -0300

    feat: show posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,45 @@ import styles from './App.module.css';
 import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/53883371?v=4',
+      name: 'Felipe Ribeiro',
+      role: 'Front-End Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          '  Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-07 20:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/53883371?v=4',
+      name: 'John Doe',
+      role: 'Front-End Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          '  Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-09 21:00'),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -10,8 +49,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                publishedAt={post.publishedAt}
+                content={post.content}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
